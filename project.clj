9dd3837913f461d3a6ed6ab70e44c3dbf99aaf9b
Kamil Toman <kamil.toman@leafclick.com>
2015-12-09 19:41:24 +0100

    deps update, inc. Clojure 1.7.0, ClojureScript 1.7.170, Om 0.9.0

--- project.clj
+++ project.clj
@@ -2,34 +2,34 @@
   :description "Omlab playground"
   :url "https://github.com/katox/omlab"
 
-  :dependencies [[org.clojure/clojure "1.7.0-beta2"]
-                 [compojure "1.3.3"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [compojure "1.4.0"]
                  [hiccup "1.0.5"]
-                 [cheshire "5.4.0"]
+                 [cheshire "5.5.0"]
                  [slingshot "0.12.2"]
-                 [environ "1.0.0"]
+                 [environ "1.0.1"]
                  [ring-server "0.4.0" :exclusions [org.clojure/core.incubator]]
                  [ring-basic-authentication "1.0.5"]
                  [com.cemerick/friend "0.2.0" :exclusions [org.clojure/core.cache commons-codec]]
-                 [clj-time "0.9.0"]
-                 [com.taoensso/timbre "3.4.0"]
-                 [com.datomic/datomic-free "0.9.5153" :exclusions [org.clojure/clojure com.google.guava/guava commons-codec joda-time]]
+                 [clj-time "0.11.0"]
+                 [com.taoensso/timbre "4.1.4"]
+                 [com.datomic/datomic-free "0.9.5344" :exclusions [org.clojure/clojure com.google.guava/guava commons-codec joda-time]]
                  [org.clojure/core.match "0.2.2"]
-                 [ring-transit "0.1.3"]
+                 [ring-transit "0.1.4"]
                  ;; CLJS
-                 [org.clojure/clojurescript "0.0-3211"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [org.clojure/core.async "0.2.374"]
+                 [org.clojure/clojurescript "1.7.170"]
                  [secretary "1.2.3"]
-                 [cljs-http "0.1.30" :exclusions [commons-codec com.cemerick/austin]]
-                 [org.omcljs/om "0.8.8"]
-                 [inflections "0.9.13" :exclusions [commons-codec org.clojure/clojurecommons-codec com.cemerick/austin]]
+                 [cljs-http "0.1.38" :exclusions [commons-codec com.cemerick/austin]]
+                 [org.omcljs/om "0.9.0"]
+                 [inflections "0.10.0" :exclusions [org.clojure/clojure commons-codec]]
                  ;; TEST
                  [org.clojars.runa/conjure "2.2.0" :scope "test"]
                  [org.clojure/test.generative "0.5.2" :scope "test"]]
 
   :plugins [[lein-ring "0.9.1"]
             [lein-environ "1.0.0"]
-            [lein-cljsbuild "1.0.4"]]
+            [lein-cljsbuild "1.1.1"]]
   
   :uberjar-name "omlab-standalone.jar"
   :ring {:handler omlab.handler/app
